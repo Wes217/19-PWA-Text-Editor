8954b69619737b94600c257aa9b99f4d07b6b142
Lendo <wesreedbusiness@gmail.com>
2023-08-09 23:25:47 -0500

    manfest plugin and install.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 
 module.exports = () => {
@@ -25,7 +24,32 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      new MiniCssExtractPlugin()
+
+      new MiniCssExtractPlugin(),
+
+      // new InjectManifest({
+      //   swSrc: 'src-sw.js'
+      // }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    
     ],
 
     module: {
